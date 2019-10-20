2ff0eb3af4014c774e3cfeab48485c5741832156
nersus15 <fathur.ashter15@gmail>
2019-10-20 20:10:56 +0800

    menambahkan file konfigurasi dan menerapkannya

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,15 +6,18 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.gt('jwt');
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([UserRepository]),
     JwtModule.register({
-      secret: 'Secret1024',
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
       signOptions: {
-        expiresIn: 3600,
+        expiresIn: jwtConfig.expiresIn,
 
       }
     }),
