e375d2fc6b1e479468421d13a8ead4cfa63dc6d7
fathurrahman <fathur.ashter15@gmail.com>
2019-11-20 13:03:45 +0800

    change port (configuration)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import * as config from 'config';
 
-const jwtConfig = config.gt('jwt');
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
